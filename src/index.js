3ee07c141430979f70718ec940b5cc78cfb23316
quang-nq <nguyenquangns1@gmail.com>
2023-09-10 19:10:57 +0700

    Add New news page

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ const handlebars = require('express-handlebars')
 const app = express();
 const port = 3000;
 
+//Body-parser
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, '\\public')));
 //HTTP Request
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Handlebar
 app.engine('handlebars', handlebars.engine());
@@ -17,6 +21,19 @@ app.set('views', path.join(__dirname, 'resource\\views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  res.send('');
+  console.log(req.body);
 })
 
 app.listen(port, () => {
